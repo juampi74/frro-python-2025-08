1da335746f12125179a15f380bc330f993b5c33d
na04ber <nahuel.berli@gmail.com>
2025-07-26 18:17:40 -0300

    evito tener que descargar el .geojson

--- TPI/GIS Nuevo/departamento.py
+++ TPI/GIS Nuevo/departamento.py
@@ -1,21 +1,28 @@
 ##Tener descargado y en la misma carpeta el: Poligono Departamento Shapefile de la pagina: https://www.ign.gob.ar/NuestrasActividades/InformacionGeoespacial/CapasSIG
-##1- entrar a https://geojson.io/
-##2- dibujar el poligono
-##3- Descargar el geojson y ponerlo en la misma carpeta que este .py
+##Las coordenadas que da el mapa interactivo copiarlas en la variable geojson
+import json
 import geopandas as gpd
+from shapely.geometry import shape
 
-# Leer el polígono desde tu archivo geojson
-poligono = gpd.read_file("map.geojson")
+geojson = """
+{"type":"Feature","properties":{},"geometry":{"type":"Polygon","coordinates":[[[-64.337324,-33.142681],[-64.336681,-33.142834],[-64.336981,-33.143355],[-64.337625,-33.14322],[-64.337324,-33.142681]]]}}
+"""
+geojson_dict = json.loads(geojson)
+
+# Convertir a geometría de shapely
+mi_poligono = shape(geojson_dict["geometry"])
+
+# Crear un GeoDataFrame con esa geometría
+poligono_gdf = gpd.GeoDataFrame(index=[0], geometry=[mi_poligono], crs="EPSG:4326")
 
 # Leer los departamentos desde el shapefile del IGN
 departamentos = gpd.read_file("departamentoPolygon.shp")
 
 # Asegurarse que ambos estén en el mismo sistema de coordenadas (WGS84 - EPSG:4326)
-poligono = poligono.to_crs(epsg=4326)
 departamentos = departamentos.to_crs(epsg=4326)
 
 # Hacer la intersección: encontrar qué departamentos intersectan con tu polígono
-interseccion = gpd.overlay(departamentos, poligono, how="intersection")
+interseccion = gpd.overlay(departamentos, poligono_gdf, how="intersection")
 
 ##print("----------------------------------")
 ##print(departamentos.columns)
